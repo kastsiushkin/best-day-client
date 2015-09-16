18d0356ae2155b9e8e0271aec07ced7b874014ae
kastsiushkin <alexkastsiushkin@gmail.com>
2015-09-16 09:50:50 -0700

    Add test

--- tests/unit/pods/day/model-test.js
+++ tests/unit/pods/day/model-test.js
@@ -1,12 +1,19 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('day', 'Unit | Model | day', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:activity']
 });
 
-test('it exists', function(assert) {
-  var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+test('Day model exists', function(assert) {
+  var day = this.subject();
+  assert.ok(!!day);
+});
+
+test('activity relationship', function(assert) {
+  var Day = this.store().modelFor('day');
+  var relationship = Ember.get(Day, 'relationshipsByName').get('activities');
+  assert.equal(relationship.key, 'activities', 'Relationship key is "activities"');
+  assert.equal(relationship.kind, 'hasMany', "Relationship kind is hasMany");
 });
---
+++ tests/unit/pods/days/index/route-test.js
@@ -0,0 +1,8 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:days/index', 'Unit | Route | days');
+
+test('days/index route exists', function(assert) {
+  var route = this.subject();
+  assert.ok(!route, 'days/index route exists');
+});
